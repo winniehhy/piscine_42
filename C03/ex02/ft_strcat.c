4949165d5b17d3c30791439767b26fdd385ae49a
wiwi_hhy <155449474+winniehhy@users.noreply.github.com>
2024-02-14 16:57:04 +0800

    Create ft_strcat.c

---
+++ C03/ex02/ft_strcat.c
@@ -0,0 +1,46 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_strcat.c                                        :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: hheng <hheng@student.42kl.edu.my>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2024/01/22 11:17:39 by hheng             #+#    #+#             */
+/*   Updated: 2024/01/22 13:37:45 by hheng            ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+//#include <stdio.h>
+//#include <string.h>
+
+char	*ft_strcat(char *dest, char *src)
+{
+	int	i;
+	int	j;
+
+	i = 0;
+	j = 0;
+	while (dest[i] != '\0')
+	{
+		i++;
+	}
+	while (src[j] != '\0')
+	{
+		dest[i] = src[j];
+		i++;
+		j++;
+	}
+	dest[i] = '\0';
+	return (dest);
+}
+
+/*int main()
+{
+	char dest[20] = "Hello, ";
+	char src[] = "Friend";
+	unsigned int nb = 6;
+
+	ft_strncat(dest, src, nb);
+	printf("Concatenated Sring: %s\n",dest);
+	return 0;
+}*/
