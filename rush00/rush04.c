70743dd2c7a40d84404931bff25903bd7ba57d18
wiwi_hhy <155449474+winniehhy@users.noreply.github.com>
2024-02-14 17:21:04 +0800

    Create rush04.c

---
+++ rush00/rush04.c
@@ -0,0 +1,106 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   rush04.c                                           :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: bng <bng@student.42kl.edu.my>              +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2024/01/13 12:13:02 by bng               #+#    #+#             */
+/*   Updated: 2024/01/13 17:28:30 by bng              ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+#include <unistd.h>
+
+void	ft_putchar(char c);
+
+void	first_line(int x)
+{
+	int	number;
+
+	number = 0;
+	while (number < x)
+	{
+		if (number == 0)
+		{
+			ft_putchar('A');
+		}
+		else if (number == x - 1)
+		{
+			ft_putchar('C');
+		}
+		else
+		{
+			ft_putchar('B');
+		}
+		number++;
+	}
+}
+
+void	body_line(int x)
+{
+	int	number;
+
+	number = 0;
+	while (number < x)
+	{
+		if (number == 0 || number == x - 1)
+		{
+			ft_putchar('B');
+		}
+		else
+		{
+			ft_putchar(' ');
+		}
+		number++;
+	}
+}
+
+void	final_line(int x)
+{
+	int	number;
+
+	number = 0;
+	while (number < x)
+	{
+		if (number == 0)
+		{
+			ft_putchar('C');
+		}
+		else if (number == x - 1)
+		{
+			ft_putchar('A');
+		}
+		else
+		{
+			ft_putchar('B');
+		}
+		number++;
+	}
+}
+
+void	rush(int x, int y)
+{
+	int	numbery;
+
+	if (x <= 0 || y <= 0)
+		return ;
+	numbery = 0;
+	if (numbery == 0)
+	{
+		first_line(x);
+		numbery++;
+		ft_putchar('\n');
+	}
+	while (numbery > 0 && numbery < y - 1)
+	{
+		body_line(x);
+		numbery++;
+		ft_putchar('\n');
+	}
+	if (numbery == y - 1)
+	{
+		final_line(x);
+		numbery++;
+		ft_putchar('\n');
+	}
+}
