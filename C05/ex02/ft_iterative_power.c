9d4413d90063bc73c14b6410cd523eb40da519e8
wiwi_hhy <155449474+winniehhy@users.noreply.github.com>
2024-02-14 17:06:01 +0800

    Create  ft_iterative_power.c

---
+++ C05/ex02/ft_iterative_power.c
@@ -0,0 +1,31 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_iterative_power.c                               :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: hheng <hheng@student.42kl.edu.my>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2024/01/25 13:53:21 by hheng             #+#    #+#             */
+/*   Updated: 2024/01/25 15:09:01 by hheng            ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+//#include <stdio.h>
+
+int	ft_iterative_power(int nb, int power)
+{
+	int	result;
+
+	if (power < 1)
+		return (!power);
+	result = nb;
+	while (--power)
+		result *= nb;
+	return (result);
+}
+
+/*int	main(void)
+{
+	printf("OUTPUT: %d\n", ft_iterative_power(3, 4));
+	return (0);
+}*/
