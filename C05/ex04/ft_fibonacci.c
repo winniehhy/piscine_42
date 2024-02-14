f17db812efc4798844f180f699481a5cfb6add37
wiwi_hhy <155449474+winniehhy@users.noreply.github.com>
2024-02-14 17:06:47 +0800

    Create  ft_fibonacci.c

---
+++ C05/ex04/ft_fibonacci.c
@@ -0,0 +1,30 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_fibonacci.c                                     :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: hheng <hheng@student.42kl.edu.my>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2024/01/25 17:42:09 by hheng             #+#    #+#             */
+/*   Updated: 2024/01/25 18:04:23 by hheng            ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+//#include <stdio.h>
+
+int	ft_fibonacci(int index)
+{
+	if (index < 0)
+		return (-1);
+	if (index == 0)
+		return (0);
+	if (index == 1)
+		return (1);
+	return (ft_fibonacci(index - 1) + ft_fibonacci(index - 2));
+}
+
+/*int	main(void)
+{
+	printf("OUTPUT: %d\n", ft_fibonacci(6));
+    return (0);
+}*/
