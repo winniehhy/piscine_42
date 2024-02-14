c2923131f89e21cfe8f8c5cc461a00300b83b266
wiwi_hhy <155449474+winniehhy@users.noreply.github.com>
2024-02-14 16:44:52 +0800

    Create  ft_print_comb2.c

---
+++ C00/ex06/ft_print_comb2.c
@@ -0,0 +1,47 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_print_comb2.c                                   :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: hheng <hheng@student.42kl.edu.my>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2024/01/11 14:48:10 by hheng             #+#    #+#             */
+/*   Updated: 2024/01/11 14:48:10 by hheng            ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include <unistd.h>
+
+void	ft_putchar(char c)
+{
+	write(1, &c, 1);
+}
+
+void	ft_print_comb(int a, int b)
+{
+	ft_putchar(a / 10 + 48);
+	ft_putchar(a % 10 + 48);
+	write(1, " ", 1);
+	ft_putchar(b / 10 + 48);
+	ft_putchar(b % 10 + 48);
+}
+
+void	ft_print_comb2(void)
+{
+	int	a;
+	int	b;
+
+	a = 0;
+	while (a <= 98)
+	{
+		b = a +1;
+		while (b <= 99)
+		{
+			ft_print_comb(a, b);
+			if (a != 98 || b != 99)
+				write(1, ", ", 2);
+			b++;
+		}
+		a++;
+	}
+}
