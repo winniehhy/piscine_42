83a0e1132e625878fd0062c57e3f4f192c5d8812
wiwi_hhy <155449474+winniehhy@users.noreply.github.com>
2024-02-14 17:11:54 +0800

    Create ft_ultimate_range.c

---
+++ C07/ex02/ft_ultimate_range.c
@@ -0,0 +1,55 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_ultimate_range.c                                :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: hheng <hheng@student.42kl.edu.my>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2024/01/29 17:52:40 by hheng             #+#    #+#             */
+/*   Updated: 2024/01/29 18:07:56 by hheng            ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include <stdio.h>
+#include <stdlib.h>
+
+int	ft_ultimate_range(int **range, int min, int max)
+{
+	int	i;
+
+	if (min >= max)
+	{
+		*range = NULL;
+		return (0);
+	}
+	*range = malloc(sizeof(int) * (max - min));
+	if (*range == NULL)
+		return (-1);
+	i = 0;
+	while (i < (max - min))
+	{
+		(*range)[i] = min + i;
+		i++;
+	}
+	return (i);
+}
+
+/*int	main(void)
+{
+	int	*range;
+	int	min;
+	int	max;
+	int	i;
+
+	min = -5;
+	max = 10;
+	printf ("SIZE: %d\n", ft_ultimate_range(&range, min, max));
+	i = 0;
+	printf ("OUTPUT\n");
+	while (i < (max - min))
+	{
+		printf ("%d", range[i]);
+		i++;
+	}
+	return (0);
+}*/
