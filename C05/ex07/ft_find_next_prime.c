2988927bf20e435000914beb6c7f0a52b5b38f3f
wiwi_hhy <155449474+winniehhy@users.noreply.github.com>
2024-02-14 17:08:09 +0800

    Create ft_find_next_prime.c

---
+++ C05/ex07/ft_find_next_prime.c
@@ -0,0 +1,53 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_find_next_prime.c                               :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: hheng <hheng@student.42kl.edu.my>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2024/01/27 15:55:50 by hheng             #+#    #+#             */
+/*   Updated: 2024/01/27 15:59:14 by hheng            ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+//#include <stdio.h>
+
+int	ft_is_prime(int nb)
+{
+	int	i;
+
+	i = 2;
+	if (nb < 2)
+	{
+		return (0);
+	}
+	while (i * i <= nb)
+	{
+		if (nb % i == 0)
+		{
+			return (0);
+		}
+		i++;
+	}
+	return (1);
+}
+
+int	ft_find_next_prime(int nb)
+{
+	while (!ft_is_prime(nb))
+	{
+		nb++;
+	}
+	return (nb);
+}
+
+/*int main() {
+    int num = 15;
+
+    int next_prime = ft_find_next_prime(num);
+
+    printf("Next prime number greater or equal to %d is: %d\n", num, next_prime);
+
+    return 0;
+}
+*/
