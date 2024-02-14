c48b5cea74977b0db304c135924f3719770dc8f5
wiwi_hhy <155449474+winniehhy@users.noreply.github.com>
2024-02-14 16:52:58 +0800

    Create ft_strupcase.c

---
+++ C02/ex07/ft_strupcase.c
@@ -0,0 +1,40 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_strupcase.c                                     :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: hheng <hheng@student.42kl.edu.my>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2024/01/17 10:42:05 by hheng             #+#    #+#             */
+/*   Updated: 2024/01/17 10:42:13 by hheng            ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include <unistd.h>
+//#include <stdio.h>
+
+char	*ft_strupcase(char *str)
+{
+	int	i;
+
+	i = 0;
+	while (str[i] != '\0')
+	{
+		if (str[i] >= 'a' && str[i] <= 'z')
+		{
+			str[i] -= 32;
+		}
+		i++;
+	}
+	return (str);
+}
+
+/*int main()
+{
+	char text[5] = "bello";
+	printf("Before : %s\n", text);
+
+	ft_strupcase(text);
+	printf("After : %s", text);
+	return 0;
+}*/
