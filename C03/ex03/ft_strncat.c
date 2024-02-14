dc305d6ec07e6ee67e75aba15511bd73de097cfb
wiwi_hhy <155449474+winniehhy@users.noreply.github.com>
2024-02-14 16:58:04 +0800

    Update ft_strncat.c

--- C03/ex03/ft_strncat.c
+++ C03/ex03/ft_strncat.c
@@ -1,1 +1,41 @@
-ft_strcat.c
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_strncat.c                                       :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: hheng <hheng@student.42kl.edu.my>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2024/01/22 13:43:22 by hheng             #+#    #+#             */
+/*   Updated: 2024/01/22 16:18:48 by hheng            ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+//#include <stdio.h>
+//#include <string.h>
+
+char	*ft_strncat(char *dest, char *src, unsigned int nb)
+{
+	unsigned int	i;
+	unsigned int	j;
+
+	i = 0;
+	j = 0;
+	while (dest[i] != '\0')
+		i++;
+	while (src[j] != '\0' && j < nb)
+	{
+		dest[i + j] = src[j];
+		j++;
+	}
+	dest[i + j] = '\0';
+	return (dest);
+}
+/*int     main()
+{
+	char    text1[20] = "SALUTWSH"; //dest
+	char    text2[] = "TEST"; //src
+
+	ft_strncat(text1, text2, 2);
+	printf("OUTPUT: %s\n", text1);
+	return (0);
+}*/
