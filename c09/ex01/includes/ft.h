68ee263966bef9a31a0ec54c46a4dc010c216e2a
wiwi_hhy <155449474+winniehhy@users.noreply.github.com>
2024-02-24 13:22:45 +0800

    ex01
    add ex01

---
+++ c09/ex01/includes/ft.h
@@ -0,0 +1,25 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft.h                                               :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: hheng <hheng@student.42kl.edu.my>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2024/02/24 12:51:20 by hheng             #+#    #+#             */
+/*   Updated: 2024/02/24 12:55:29 by hheng            ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifndef FT_H
+# define FT_H
+
+void	ft_putchar(char c);
+
+void	ft_swap(int *a, int *b);
+
+void	ft_putstr(char *str);
+
+int		ft_strlen(char *str);
+
+int		ft_strcmp(char *s1, char *s2);
+#endif
